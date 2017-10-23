8053c8c2ed38494cf176bf3a6c0f7266a7b52cfd
EduardoCarvajal <edu.carvajal.t@gmail.com>
2017-10-22 23:47:36 -0400

    importacion de materiales de Angular. (falta conf. el routing)

--- noticias/src/app/app.module.ts
+++ noticias/src/app/app.module.ts
@@ -3,12 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+import {
+  MatListModule,
+  MatInputModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatCheckboxModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatChipsModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatTabsModule,
+  MatGridListModule
+} from '@angular/material';
+
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatInputModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatGridListModule,
+    routing
+
   ],
   providers: [],
   bootstrap: [AppComponent]
