2a77454b1a74aed7c600450558863a555cd8eca5
Luis San Martín <lsansaa@gmail.com>
2017-11-07 01:16:28 -0300

    Implementación de los botones de favoritos para trabajar en conjunto con el servicio

--- noticias/src/app/components/home/home.component.ts
+++ noticias/src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HackerNewsService } from "../../services/hacker-news.service";
+import { FavoritesService} from "../../services/favorites.service";
+
+import { News} from "../../model/news";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-home',
@@ -9,11 +13,33 @@ import { HackerNewsService } from "../../services/hacker-news.service";
 })
 export class HomeComponent implements OnInit {
   news = {};
-  constructor(private HackerNewsService: HackerNewsService) {
+  constructor(private HackerNewsService: HackerNewsService, private FavoritesService: FavoritesService) {
     this.HackerNewsService.getNews().subscribe(data => this.news = data);
   }
 
   ngOnInit() {
+  }
+
+  addFavorite(publishedAt){
+    let favorite = {};
+    console.log(publishedAt)
+    let articles = this.news['articles'];
+    for(let article in articles){
+      if (articles[article]['publishedAt'] == publishedAt){
+        console.log(articles[article]['author']);
+        favorite = new News(
+          articles[article]['publishedAt'],
+          articles[article]['title'],
+          articles[article]['description'],
+          articles[article]['author'],
+          articles[article]['url'],
+          articles[article]['urlToImage']
+        );
+        this.FavoritesService.addFavorite(favorite);
+        break;
+      }
+    }
+
   }
 
 }
