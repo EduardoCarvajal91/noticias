d7d83d3284e8e38be7497d288f4e5649431f9c4b
Luis San Martín <lsansaa@gmail.com>
2017-11-07 01:13:13 -0300

    Creación  de la clase de noticias, news, que contiene fecha de publicacion, autor, descripcion y titulo

--- noticias/src/app/model/news.ts
+++ noticias/src/app/model/news.ts
@@ -1,6 +1,10 @@
 export class News {
   constructor(
+    public publishedAt: string,
     public title: string,
-    public text: string,
-    public author: string) { }
+    public description: string,
+    public author: string,
+    public url: string,
+    public urlToImage
+    ) { }
 }
